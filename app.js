80837801724171e48a876900d98c542ccb07f69a
yahya khalaf <yahya.khalaf54@gmail.com>
2025-03-02 11:56:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// Simple Node.js "Hello World" app
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Yahya Saying Hello there from TeleMedPilot Lab!');
